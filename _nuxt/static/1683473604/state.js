a836d23bbb80b9f3a8094daf2970dd3750e3a939
Fariz Rizky Rizaldy <farizrizkyrizaldy@gmail.com>
2023-05-07 22:35:02 +0700

    8071ec5

---
+++ _nuxt/static/1683473604/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fluwjistik-testcase\u002F_nuxt\u002Fstatic\u002F1683473604",layout:"default",error:a,state:{signin:{}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fluwjistik-testcase\u002F",assetsPath:"\u002Fluwjistik-testcase\u002F_nuxt\u002F",cdnURL:a}}}}(null));
